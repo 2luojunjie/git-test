0a2e3ee68d73d4a204483b62e91ea883bd303487
Junjie <26245515531@qq.com>
2024-06-05 23:11:34 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;
+
+/**
+ * ClassName: gitTest
+ * Package: com.atguigu.git
+ *
+ * @author: 罗骏杰
+ * @create: 2024-06-05 23:03
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+    }
+}
